01af5c362e91f7eca7ebed61858322741029b423
aya salama <asalama0204@gmail.com>
2026-06-09 01:52:51 -0400

    fix lexer tests based on new escaping implementation

--- tests/lexer_test.rs
+++ tests/lexer_test.rs
@@ -5,20 +5,20 @@ fn amount_of_tokens() {
     let input = String::from("echo this is a test \\n\\t >> somefile");
     let result = lex(&input);
     match result {
-        Ok(r) => assert_eq!(9, r.len()),
+        Ok(r) => assert_eq!(8, r.len()),
         Err(_) => eprintln!("error getting the return value."),
     }
 }
 
 #[test]
 fn token_types() {
-    let input = String::from("echo this is a test \\n $PATH\\t >> somefile");
+    let input = String::from("echo this is a test \\n\\& $PATH\\t >> somefile");
     let result = lex(&input);
     match result {
         Ok(r) => {
             let output = format!("{:?}", r);
             assert_eq!(
-                r#"[Id("echo"), Id("this"), Id("is"), Id("a"), Id("test"), Escape("n"), Dollar, Id("PATH"), Escape("t"), RedirectOutputAppend, Id("somefile")]"#,
+                r#"[Id("echo"), Id("this"), Id("is"), Id("a"), Id("test"), Id("n&"), Dollar, Id("PATHt"), RedirectOutputAppend, Id("somefile")]"#,
                 output
             )
         }
