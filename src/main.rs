0183c7de8a4861b1495fd499e472a882df56ae93
aya salama <asalama0204@gmail.com>
2026-06-09 23:07:24 -0400

    refactor main

--- src/main.rs
+++ src/main.rs
@@ -4,45 +4,52 @@ use rshell::parser::parse;
 use std::io;
 use std::io::Write;
 
+fn handle_input() {
+    let mut input = String::new();
+
+    loop {
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        let mut input_cloned = input.clone();
+        input_cloned = input_cloned.trim().to_string();
+        if input_cloned.is_empty() {
+            return;
+        }
+
+        let last_char = input_cloned.chars().nth(input_cloned.len() - 1).unwrap();
+        if last_char == '\\' {
+            let second_to_last_char = input_cloned.chars().nth(input_cloned.len() - 2).unwrap();
+            if second_to_last_char == '\\' {
+                break;
+            }
+
+            input_cloned.pop();
+            input = input_cloned;
+            print!("> ");
+            io::stdout().flush().unwrap();
+        } else {
+            break;
+        }
+    }
+
+    let lex_result = lex(&input.to_string());
+    match lex_result {
+        Err(e) => {
+            eprintln!("{e}");
+        }
+        Ok(tokens) => {
+            let _expr = parse(&tokens);
+        }
+    }
+}
+
 fn main() {
     loop {
         print!("rshell$ ");
         io::stdout().flush().unwrap();
 
-        let mut input = String::new();
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read line");
-
-        loop {
-            input = input.trim().to_string();
-            let last_char = input.chars().nth(input.len() - 1).unwrap();
-            if last_char == '\\' {
-                let second_to_last_char = input.chars().nth(input.len() - 2).unwrap();
-                if second_to_last_char == '\\' {
-                    break;
-                }
-
-                input.pop();
-                print!("> ");
-                io::stdout().flush().unwrap();
-
-                io::stdin()
-                    .read_line(&mut input)
-                    .expect("Failed to read line");
-            } else {
-                break;
-            }
-        }
-
-        let lex_result = lex(&input.to_string());
-        match lex_result {
-            Err(e) => {
-                eprintln!("{e}");
-            }
-            Ok(tokens) => {
-                let _expr = parse(&tokens);
-            }
-        }
+        handle_input();
     }
 }
